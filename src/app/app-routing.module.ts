11ac0b7baeb9801837f500608811c9123f152852
Royal6969 <sergiodiazcampos@gmail.com>
2021-12-13 10:15:24 +0100

    routes and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailsComponent
+  },
+  {
+    path: 'add-book',
+    component: AddBookComponent
+  },
+  {
+    path: 'edit-book/:id',
+    component: EditBookComponent
+  },
+  {
+    path: 'delete-book/:id',
+    component: DeleteBookComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
